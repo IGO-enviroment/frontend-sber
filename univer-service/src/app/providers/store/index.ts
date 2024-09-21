c948c030a4bc68254e8d770fe0304a6ab8339829
DmitryOvechkin <ovechkin_ds@ekb.sima-land.ru>
2024-09-21 13:42:41 +0500

    init

--- univer-service/src/app/providers/store/index.ts
+++
@@ -1,2 +0,0 @@
-export { StoreProvider } from './ui';
-export { createReduxStore } from './config/store';
--- univer-service/src/app/providers/theme/index.ts
+++
@@ -1,3 +0,0 @@
-export { ThemeProvider } from './ui/provider';
-export { useTheme } from './lib/use-theme';
-export { Theme } from './lib/context';
